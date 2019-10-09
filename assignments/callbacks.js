7511f7e780babf844970773eebb9ed74b0962829
Adrian Guadalupe <safisweetkeyz@gmail.com>
2019-10-08 22:52:18 -0400

    stuck on callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -6,15 +6,15 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
   // GIVEN THIS PROBLEM:
 
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
+  // function firstItem(arr, cb) {
+  //   // firstItem passes the first item of the given array to the callback function.
+  // }
 
   // SOLUTION:
 
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
+  // function firstItem(arr, cb) {
+  //   return cb(arr[0]);
+  // }
 
   // NOTES ON THE SOLUTION:
 
@@ -41,24 +41,57 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
+
+getLength(items, (length) => {
+  console.log(length);
+});
+
+
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr[3]);
 }
+
+last(items, (lastItem) => {
+  console.log(lastItem);
+});
+
+
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 }
+
+sumNums
+
+
+
+
+
+
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
 }
 
+
+
+
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
+
+
+
+
 
 /* STRETCH PROBLEM */
 
